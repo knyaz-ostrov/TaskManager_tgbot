926dd5790f46d3e56de1067f591d8ec0dfe324ed
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-05-08 16:40:33 +0500

    Смена имен переменных окружения

--- misc/config.py
+++ misc/config.py
@@ -3,27 +3,27 @@ import os
 class BotConfig:
 
     def get_token() -> str:
-        token = os.environ.get('TOKEN')
+        token = os.environ.get('STaskMgr_tgbot_BOT_TOKEN')
         return token
 
 class PostgresData:
 
     def get_db_name() -> str:
-        database = os.environ.get('TEST_PSQL_DATABASE')
+        database = os.environ.get('STaskMgr_tgbot_DB_DATABASE')
         return database
     
     def get_db_user() -> str:
-        user = os.environ.get('TEST_PSQL_USER')
+        user = os.environ.get('STaskMgr_tgbot_DB_USER')
         return user
     
     def get_db_password() -> int:
-        password = int(os.environ.get('TEST_PSQL_PASSWORD'))
+        password = int(os.environ.get('STaskMgr_tgbot_DB_PASSWORD'))
         return password
     
     def get_db_host() -> str:
-        host = os.environ.get('TEST_PSQL_HOST')
+        host = os.environ.get('STaskMgr_tgbot_DB_HOST')
         return host
     
     def get_db_port() -> str:
-        port = os.environ.get('TEST_PSQL_PORT')
+        port = os.environ.get('STaskMgr_tgbot_DB_PORT')
         return port
