8b1fac9ff4add880d0f87a9325de32e609ea5820
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-05-16 18:59:47 +0500

    Смена каталога

--- RecreateDB.py
+++ RecreateDB.py
@@ -1,9 +1,9 @@
-from scripts.db_shell import DBCreator
+from scripts.db_shell import DBRecreator
 
 def main():
     confirm = input('Вы действительно хотите выполнить пересоздание БД? [Y/N]: ').lower()
     if confirm == 'y':
-        DBCreator()
+        DBRecreator()
 
 if __name__ == '__main__':
     main()
