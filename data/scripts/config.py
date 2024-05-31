7cb0210aff6294f6887fc6646d3a054c07443a5a
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-01 03:18:04 +0500

    Сделал словари с конфигами приватными

--- data/scripts/config.py
+++ data/scripts/config.py
@@ -8,20 +8,18 @@ FILE_PATH = 'data/data/config.json'
 
 class BotConfig:
     with open(FILE_PATH) as file:
-        bot_config = json.load(file)['bot']
+        __bot_config = json.load(file)['bot']
 
-    token = bot_config['token']
-    del bot_config
+    token = __bot_config['token']
 
 
 
 class PSQLConfig:
     with open(FILE_PATH) as file:
-        psql_config = json.load(file)['postgresql']
+        __psql_config = json.load(file)['postgresql']
 
-    database = psql_config['database']
-    user     = psql_config['user']
-    password = psql_config['password']
-    host     = psql_config['host']
-    port     = psql_config['port']
-    del psql_config
+    database = __psql_config['database']
+    user     = __psql_config['user']
+    password = __psql_config['password']
+    host     = __psql_config['host']
+    port     = __psql_config['port']
