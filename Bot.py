ad08d69a0f017c883e4d4a8095e2a9cd66905057
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-03 17:56:53 +0500

    Добавил логгирование

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 
@@ -13,11 +14,18 @@ async def main() -> None:
     dp = Dispatcher()
 
     dp.include_router(general.router)
-
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="log.log",
+        filemode="w",
+        format="%(asctime)s %(levelname)s %(message)s",
+        encoding='UTF-8'
+    )
+
     asyncio.run(main())
