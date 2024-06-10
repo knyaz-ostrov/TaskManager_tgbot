ed0e5d2db25140ac8b24ee717db1dd0b855ac2f3
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-10 21:52:35 +0500

    Рефакторинг и комментарии

--- data/scripts/bot_data.py
+++ data/scripts/bot_data.py
@@ -1,17 +1,21 @@
+"""
+Модуль для текста, который используется в функционале бота.
+"""
 import lxml.etree as ET
 
 
-
 FILE_PATH = 'data/data/bot_data.xml'
-path_template = './/{}'
-filter = '[@id="{}"]'
-
+XPATH_TEMPLATE = './/{}'
+XPATH_FILTER = '[@id="{}"]'
 
 
 class BotMessageText:
+    """
+    Класс для хранения текстовых сообщений отправляемых ботом.
+    """
     bot_data = ET.parse(FILE_PATH)
     category = 'text'
-    path = path_template.format(category) + filter
+    path = XPATH_TEMPLATE.format(category) + XPATH_FILTER
 
     start           = bot_data.xpath(path.format('start'))[0].text
     add_task        = bot_data.xpath(path.format('add_task'))[0].text
@@ -22,11 +26,13 @@ class BotMessageText:
     del bot_data, category, path
 
 
-
 class BotCommands:
+    """
+    Класс для хранения команд, которые принимает бот.
+    """
     bot_data = ET.parse(FILE_PATH)
     category = 'command'
-    path = path_template.format(category) + filter
+    path = XPATH_TEMPLATE.format(category) + XPATH_TEMPLATE
 
     add_task    = bot_data.xpath(path.format('add_task'))[0].text
     get_tasks   = bot_data.xpath(path.format('get_tasks'))[0].text
