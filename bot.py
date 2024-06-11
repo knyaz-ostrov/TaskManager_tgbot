25c906428ec72ccf644c1e12127ea8e3ca0acd0c
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-12 01:58:22 +0500

    Вынес константы

--- bot.py
+++ bot.py
@@ -9,9 +9,10 @@ from aiogram import Bot, Dispatcher
 
 from handlers import general
 from data.scripts.config import BotConfig
+from constants import LOG_BOT_START, LOG_FILE_NAME, LOG_FORMAT
 
 
-@logging.info("[BOT] Попытка запуска бота с токеном: [%s]", BotConfig.token)
+@logging.info(LOG_BOT_START, BotConfig.token)
 async def main() -> None:
     """
     Указывает токен для телеграм-бота, добавляет все роутеры из директории handlers, удаляет все
@@ -30,9 +31,9 @@ async def main() -> None:
 if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
-        filename="log.log",
+        filename=LOG_FILE_NAME,
         filemode="w",
-        format="[%(asctime)s] [%(levelname)s]: %(message)s",
+        format=LOG_FORMAT,
         encoding='UTF-8'
     )
 
---
+++ constants.py
@@ -0,0 +1,6 @@
+"""
+Модуль для хранения констант.
+"""
+LOG_BOT_START = "[BOT] Попытка запуска бота с токеном: [%s]"
+LOG_FILE_NAME = "log.log"
+LOG_FORMAT = "[%(asctime)s] [%(levelname)s]: %(message)s"
