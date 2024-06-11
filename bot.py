e5c36b26229fac148be71774f03bece3d84c8800
knyaz-ostrov <72760787+knyaz-ostrov@users.noreply.github.com>
2024-06-12 00:28:24 +0500

    Комментарии, логгирование

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
+"""
+Модуль для запуска телеграм-бота.
+"""
 import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
 
 
-from data.scripts.config import BotConfig
 from handlers import general
+from data.scripts.config import BotConfig
 
 
-
+@logging.info("Попытка запуска бота с токеном: [%s]", BotConfig.token)
 async def main() -> None:
+    """
+    Указывает токен для телеграм-бота, добавляет все роутеры из директории handlers, удаляет все
+    события, произошедшие во время неактивности бота, запускает бота.
+    
+    :return:
+    """
     bot = Bot(BotConfig.token)
     dp = Dispatcher()
 
     dp.include_router(general.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-
 
 
 if __name__ == '__main__':
